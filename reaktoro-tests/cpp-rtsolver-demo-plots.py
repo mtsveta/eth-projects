3eb74d4969dbcf61c26dcd7fc95fe7dece01eb0f
Svetlana Kyas <mtsveta@gmail.com>
2019-05-13 16:21:52 +0200

    results plotting of the demo-reactivetransportsolver.cpp is extended

--- reaktoro-tests/cpp-rtsolver-demo-plots.py
+++ reaktoro-tests/cpp-rtsolver-demo-plots.py
@@ -1,4 +1,4 @@
-from numpy import *
+import numpy as np
 import matplotlib.pyplot as plt
 from joblib import Parallel, delayed
 import os
@@ -12,30 +12,34 @@ year = 365 * day
 
 # Discretisation parameters
 xl = 0.0          # the x-coordinate of the left boundary
-xr = 100.0          # the x-coordinate of the right boundary
-nsteps = 100      # the number of steps in the reactive transport simulation
-ncells = 100      # the number of cells in the discretization
+xr = 0.5          # the x-coordinate of the right boundary
+nsteps = 6000    # the number of steps in the reactive transport simulation
+ncells = 50       # the number of cells in the discretization
+reltol = 1e-1     # relative tolerance
+abstol = 1e-1     # absolute tolerance
 
 D  = 1.0e-9       # the diffusion coefficient (in units of m2/s)
 v  = 1.0/day      # the fluid pore velocity (in units of m/s)
-dt = 10*minute    # the time step (in units of s)
-T = 60.0 + 273.15 # the temperature (in units of K)
-P = 100 * 1e5     # the pressure (in units of Pa)
+dt = minute       # the time step (in units of s)
+T = 60.0          # the temperature (in units of K)
+P = 100           # the pressure (in units of Pa)
 
 dirichlet = False # the parameter that defines whether Dirichlet BC must be used
 smrt_solv = True  # the parameter that defines whether classic or smart
                   # EquilibriumSolver must be used
 
-test_tag_smart = "-ncells-" + str(ncells) + \
-                 "-nsteps-" + str(nsteps) + \
-                 "-ismart-" + "1"
+tag = "-ncells-" + str(ncells) + \
+      "-nsteps-" + str(nsteps) + \
+      "-reltol-" + "{:.{}e}".format(reltol, 1) + \
+      "-abstol-" + "{:.{}e}".format(abstol, 1)
 
-test_tag_class = "-ncells-" + str(ncells) + \
-                 "-nsteps-" + str(nsteps) + \
-                 "-ismart-" + "0"
+test_tag_smart = tag + "-smart"
+test_tag_class = tag + "-reference"
 
-folder_smart = "../results" + test_tag_smart
-folder_class = "../results" + test_tag_class
+folder_smart   = "../results" + test_tag_smart
+folder_class   = "../results" + test_tag_class
+folder_results = "results"
+folder_general = "results-" + tag
 
 # Output properties 
 output_quantities = """
@@ -59,139 +63,146 @@ indx_CO2aq     = 5
 indx_calcite   = 6
 indx_dolomite  = 7
 
-colors = ['aqua', 'darkblue',
-          'coral', 'crimson',
-          'green', 'darkgreen',
-          'orange', 'darkorange',
-          'pink', 'maroon',
-          'gold', 'brown']
+def empty_marker(color):
+    return {'facecolor':'white', 'edgecolor':color, 's': 15.0, 'zorder': 2}
 
-lines   = ['', 'solid']
-markers = ['o', '']
-markerfacecolor='orange'
+def filled_marker(color):
+    return {'color':color, 's': 15.0, 'zorder': 2}
 
-#facecolors='none'
+def line(color):
+    return {'linestyle':'-', 'color':color, 'zorder':1}
 
 def titlestr(t):
     t = t / minute   # Convert from seconds to minutes
     h = int(t) / 60  # The number of hours
     m = int(t) % 60  # The number of remaining minutes
-    return 'Time: %2d h %2d m' % (h, m)
+    return 'Time: %2dh %2dm' % (h, m)
 
-def make_results_folders(tag):
-    os.system('mkdir -p figures/ph-' + tag)
-    os.system('mkdir -p figures/aqueous-species-' + tag)
-    os.system('mkdir -p figures/calcite-dolomite-' + tag)
-    os.system('mkdir -p videos')
+def make_results_folders():
+    os.system('mkdir -p ' + folder_general + '/figures/ph')
+    os.system('mkdir -p ' + folder_general + '/figures/aqueous-species')
+    os.system('mkdir -p ' + folder_general + '/figures/calcite-dolomite')
+    os.system('mkdir -p ' + folder_general + '/videos')
+    os.system('mkdir -p ' + folder_general + '/results')
 
-def plot_chemistry(file, folder, is_smart):
+def get_step(file):
+    return int(file.split('.')[0].split('-')[1])
 
-    # Define tag based on the equilibrium solver
-    tag = "1" if is_smart == True else "0"
+def compare_chemistry(file, status):
 
     # Get the number of the step
-    step = int(file.split('.')[0].split('-')[1])
+    step = get_step(file)
 
     # Log the printing of the step
-    print('Plotting figure', step, '...')
+    # print('Plotting figure', step, '...')
 
     # The current time of the data loaded
     t = step * dt
 
     # Load the data from the filearray skipping the 1st row
-    filearray = loadtxt(folder + '/' + file, skiprows=1)
-    data = filearray.T
+    filearray_smart = np.loadtxt(folder_smart + '/' + file, skiprows=1)
+    data_smart = filearray_smart.T
+    filearray_class = np.loadtxt(folder_class + '/' + file, skiprows=1)
+    data_class = filearray_class.T
 
     # Number of digits in the total number of the steps
     ndigits = len(str(nsteps))
 
     # Cells coordinates
-    cells = linspace(xl, xr, ncells)
+    cells = np.linspace(xl, xr, ncells)
 
-    if tag == "1":
-        marker = 'o'
-        line   = ''
-    else:
-        marker = ''
-        line   = 'solid'
+    shift = 0.01 * cells[-1]
 
-    # Plot change of pH wrt the space coordinates 
+    # Plot change of pH wrt the space coordinates
     plt.figure()
-    #plt.xlim(left=-0.02, right=0.52)
-    #plt.ylim(bottom=2.5, top=10.5)
+    plt.title(titlestr(t))
+    plt.xlim(left=cells[0]-shift, right=cells[-1]+shift)
+    plt.ylim(bottom=2.5, top=10.5)
     plt.title(titlestr(t))
     plt.xlabel('Distance [m]')
     plt.ylabel('pH')
-    plt.plot(cells, data[indx_ph],
-             linestyle=line,
-             color=colors[0],
-             marker=marker,
-             markerfacecolor=colors[1] if is_smart else 'white')
-    #plt.show()
-    #plt.tight_layout()
-    plt.savefig('figures/ph-' + tag + '/{}.png'.format(str(step).zfill(
-        ndigits)))
+    ph = data_class[indx_ph]
+    plt.plot(cells, ph, **line('teal'))
+    ph = data_smart[indx_ph]
+    # status 0 - training
+    # status 1 - prediction
+    plt.scatter(cells[status[step]==0], ph[status[step]==0], **empty_marker('teal'))
+    plt.scatter(cells[status[step]==0], ph[status[step]==0], **empty_marker('teal'))
+    plt.scatter(cells[status[step]==1], ph[status[step]==1], **filled_marker('teal'))
+    plt.tight_layout()
+    plt.grid(color='lightgray', linestyle=':', linewidth=1)
+    plt.savefig(folder_general + '/figures/ph/%s.png' % (str(step).zfill(3)))
 
-    # Plot of mineral's volume the space coordinates 
+    # Plot of mineral's volume the space coordinates
     plt.figure()
-    #plt.xlim(left=-0.02, right=0.52)
-    #plt.ylim(bottom=-0.1, top=2.1)
-    plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+    plt.xlim(left=cells[0]-shift, right=cells[-1]+shift)
+    plt.ylim(bottom=-0.25, top=3.0+shift)
     plt.title(titlestr(t))
     plt.xlabel('Distance [m]')
     plt.ylabel('Mineral Volume [%$_{\mathsf{vol}}$]')
-    plt.plot(cells, data[indx_calcite] * 100, label='Calcite',
-             linestyle=line,
-             color=colors[1],
-             marker=marker,
-             markerfacecolor=colors[1] if is_smart else 'white')
-    plt.plot(cells, data[indx_dolomite] * 100, label='Dolomite',
-             linestyle=line,
-             color=colors[3],
-             marker=marker,
-             markerfacecolor=colors[3] if is_smart else 'white')
-    plt.legend(loc='center right')
-    #plt.show()
-    #plt.tight_layout()
-    plt.savefig('figures/calcite-dolomite-' + tag + '/{}.png'.format(str(step).zfill(
-        ndigits)))
 
+    data_calcite, data_dolomite = data_class[indx_calcite], data_class[indx_dolomite]
+    plt.plot(cells, data_calcite * 100, label='Calcite', **line('indianred'))
+    plt.plot(cells, data_dolomite * 100, label='Dolomite', **line('royalblue'))
+
+    data_calcite, data_dolomite  = data_smart[indx_calcite], data_smart[indx_dolomite]
+    plt.scatter(cells[status[step]==0], data_calcite[status[step]==0] * 100, **empty_marker('indianred'))
+    plt.scatter(cells[status[step]==1], data_calcite[status[step]==1] * 100, **filled_marker('indianred'))
+    plt.scatter(cells[status[step]==0], data_dolomite[status[step]==0] * 100, **empty_marker('royalblue'))
+    plt.scatter(cells[status[step]==1], data_dolomite[status[step]==1] * 100, **filled_marker('royalblue'))
+
+    plt.legend(loc='center right')
+    plt.tight_layout()
+    plt.grid(color='lightgray', linestyle=':', linewidth=1)
+    plt.savefig(folder_general + '/figures/calcite-dolomite/%s.png' % (str(step).zfill(3)))
+
+    # Plot of aqueous species's concentration the space coordinates
     plt.figure()
+    plt.xlim(left=cells[0]-shift, right=cells[-1]+shift)
+    plt.ylim(bottom=0.5e-5, top=2)
     plt.yscale('log')
-    #plt.xlim(left=-0.02, right=0.52)
-    #plt.ylim(bottom=0.5e-5, top=2)
     plt.title(titlestr(t))
     plt.xlabel('Distance [m]')
     plt.ylabel('Concentration [molal]')
-    plt.plot(cells, data[indx_Cacation], label='Ca++',
-             linestyle=line,
-             color=colors[2],
-             marker=marker,
-             markerfacecolor=colors[2] if is_smart else 'white')
-    plt.plot(cells, data[indx_Mgcation], label='Mg++',
-             linestyle=line,
-             color=colors[4],
-             marker=marker,
-             markerfacecolor=colors[4] if is_smart else 'white')
-    plt.plot(cells, data[indx_HCO3anion], label='HCO3-',
-             linestyle=line,
-             color=colors[5],
-             marker=marker,
-             markerfacecolor=colors[5] if is_smart else 'white')
-    plt.plot(cells, data[indx_CO2aq], label='CO2(aq)',
-             linestyle=line,
-             color=colors[6],
-             marker=marker,
-             markerfacecolor=colors[6] if is_smart else 'white')
-    plt.plot(cells, data[indx_Hcation], label='H+',
-             linestyle=line,
-             color=colors[7],
-             marker=marker,
-             markerfacecolor=colors[7] if is_smart else 'white')
+
+    data_cacation  = data_class[indx_Cacation]
+    data_mgcation  = data_class[indx_Mgcation]
+    data_hco3anion = data_class[indx_HCO3anion]
+    data_co2aq     = data_class[indx_CO2aq]
+    data_hcation   = data_class[indx_Hcation]
+
+    plt.plot(cells, data_cacation, label='Ca++', **line('steelblue'))
+    plt.plot(cells, data_mgcation, label='Mg++', **line('darkorange'))
+    plt.plot(cells, data_hco3anion, label='HCO3-',**line('forestgreen'))
+    plt.plot(cells, data_co2aq, label='CO2(aq)',**line('red'))
+    plt.plot(cells, data_hcation, label='H+', **line('darkviolet'))
+
+    data_cacation  = data_smart[indx_Cacation]
+    data_mgcation  = data_smart[indx_Mgcation]
+    data_hco3anion = data_smart[indx_HCO3anion]
+    data_co2aq     = data_smart[indx_CO2aq]
+    data_hcation   = data_smart[indx_Hcation]
+
+    plt.scatter(cells[status[step]==0], data_cacation[status[step]==0], **empty_marker('steelblue'))
+    plt.scatter(cells[status[step]==1], data_cacation[status[step]==1], **filled_marker('steelblue'))
+
+    plt.scatter(cells[status[step]==0], data_mgcation[status[step]==0], **empty_marker('darkorange'))
+    plt.scatter(cells[status[step]==1], data_mgcation[status[step]==1], **filled_marker('darkorange'))
+
+    plt.scatter(cells[status[step]==0], data_hco3anion[status[step]==0], **empty_marker('forestgreen'))
+    plt.scatter(cells[status[step]==1], data_hco3anion[status[step]==1], **filled_marker('forestgreen'))
+
+    plt.scatter(cells[status[step]==0], data_co2aq[status[step]==0], **empty_marker('red'))
+    plt.scatter(cells[status[step]==1], data_co2aq[status[step]==1], **filled_marker('red'))
+
+    plt.scatter(cells[status[step]==0], data_hcation[status[step]==0], **empty_marker('darkviolet'))
+    plt.scatter(cells[status[step]==1], data_hcation[status[step]==1], **filled_marker('darkviolet'))
+
     plt.legend(loc='lower right')
-    #plt.show()
-    #plt.tight_layout()
-    plt.savefig('figures/aqueous-species-' + tag + '/{}.png'.format(str(step).zfill(ndigits)))
+    plt.tight_layout()
+    plt.grid(color='lightgray', linestyle=':', linewidth=1)
+
+    plt.savefig(folder_general + '/figures/aqueous-species/%s.png' % (str(step).zfill(3)))
 
     plt.close('all')
 
@@ -210,7 +221,7 @@ def plot_cpu_times(files):
         is_smart  = file[1]
 
         folder = folder_smart if is_smart else folder_class
-        data_ = loadtxt(folder + '/' + file_name)
+        data_ = np.loadtxt(folder + '/' + file_name)
         if  "RT" in file_name and is_smart == 1:
             indx = indx_rt_smart
         elif "EQ" in file_name and is_smart == 1:
@@ -220,45 +231,105 @@ def plot_cpu_times(files):
         elif "EQ" in file_name and is_smart == 0:
             indx = indx_eq_class
         data[indx] = data_.T
-    time = linspace(0, dt * nsteps / minute, nsteps + 1)
+    time = np.linspace(0, dt * nsteps / minute, nsteps + 1)
 
     # Plot change of pH wrt the space coordinates
     plt.figure()
-    #plt.xlim(left=-0.02, right=0.52)
+    plt.xlim(left=0, right=time[-1])
     #$plt.ylim(bottom=2.5, top=10.5)
     #plt.title(titlestr(t))
     plt.xlabel('Time (minute)')
     plt.ylabel('CPU time (seconds)')
-    plt.plot(time, data[indx_rt_class], label="Reactive transport",
-             color=colors[1])
-    plt.plot(time, data[indx_eq_smart], label="Equlibrium (Smart)",
-             color=colors[3])
     plt.plot(time, data[indx_eq_class], label="Equlibrium (Conventional)",
-             color=colors[5])
-    plt.legend(loc='upper left')
-    plt.show()
-    plt.savefig('figures/cpu-time.png')
+             color='steelblue')
+    plt.plot(time, data[indx_eq_smart], label="Equlibrium (Smart)",
+             color='darkorange')
+    plt.plot(time, data[indx_rt_class], label="Reactive transport",
+             color='forestgreen')
+    plt.grid(color='lightgray', linestyle=':', linewidth=1)
+    plt.legend(loc='upper right')
+    plt.savefig(folder_general + '/figures/cpu-time.png')
+
+def count_trainings(status):
+    counter = [st for st in status if st == 0]
+    return (len(counter), len(status))
 
 if __name__ == '__main__':
 
     profiling = []
+    status    = []
 
+    make_results_folders()
+
+    # Collect files with results corresponding to smart or reference (classical) solver
+    files_smart = [file for file in sorted(os.listdir(folder_smart)) if ("profiling" not in file) and ("tracker" not in file)]
+    files_class = [file for file in sorted(os.listdir(folder_class)) if ("profiling" not in file) and ("tracker" not in file)]
+
+    # Load the status data, where 0 stands for conventional learning and 1 for smart prediction
+    status_file = [file for file in sorted(os.listdir(folder_smart)) if "status" in file]
+    if status_file != []: status = np.loadtxt(folder_smart + '/' + status_file[0])
+
+    # Collect files with profiling data
+    profiling   = profiling + [(file, 1) for file in sorted(os.listdir(folder_smart)) if "profiling" in file]
+    profiling   = profiling + [(file, 0) for file in sorted(os.listdir(folder_class)) if "profiling" in file]
+
+    # Plot profiling results
+    plot_cpu_times(profiling)
+
+    # Count the percentage of the trainings needed
+    training_counter = count_trainings(np.array(status).flatten())
+    print("%2.2f percent is training (%d learnings out of %d simulations)" % (100 * training_counter[0] / training_counter[1], training_counter[0], training_counter[1]))
+
+    # Plot comparison of the chemistry of the conventional and smart solvers
+    Parallel(n_jobs=16)(delayed(compare_chemistry)(file, status) for file in files_smart)
+
+    # Save selected plots into results folder
+    selected_steps = [10, 60, 1200, 2400, 4800]
+    for step in selected_steps:
+        copy_cmd = ('cp -i ' + folder_general + '/figures/{0}/%s.png ' + folder_general + '/results/{0}-%s-%dmin.png') % (str(step).zfill(3), str(step).zfill(3), step*dt/minute)
+        os.system(copy_cmd.format('calcite-dolomite'))
+        os.system(copy_cmd.format('ph'))
+        os.system(copy_cmd.format('aqueous-species'))
+
+    # Make  videos
+    # --------------------------------------------------------------------------
+    # Define the command for making videos
+    ffmpeg_cmd = 'ffmpeg -y -r 30 -i ' + folder_general + '/figures/{0}/%03d.png ' \
+                 '-codec:v mpeg4 -flags:v +qscale -global_quality:v 0 ' \
+                 + folder_general + '/videos/{0}.mp4'
+    os.system(ffmpeg_cmd.format('calcite-dolomite'))
+    os.system(ffmpeg_cmd.format('aqueous-species'))
+    os.system(ffmpeg_cmd.format('ph'))
+
+    """
+    for file in files_smart:
+        compare_chemistry(file, status);
+    """
+'''
     # Plot all result files
-    for is_smart in range(2):
+    for is_smart in range(1, 2):
         # Defined the tag based on the eq. solder
         tag = "1" if is_smart == True else "0"
+
         # Create folders for the plots and videos with
         make_results_folders(tag)
+
         # Defined the folder based on the eq. solver and load the files
         folder = folder_smart if is_smart else folder_class
         files = sorted(os.listdir(folder))
 
         # Collect files for profiling
-        profiling = profiling + [(file, is_smart) for file in files if
-                     "profiling" in file]
+        profiling   = profiling + [(file, is_smart) for file in files if
+                      "profiling" in file]
+        status_file = [file for file in files if is_smart and "status" in file]
+        if status_file != []:
+            status = loadtxt(folder + '/' + status_file[0])
 
-        '''
-        Parallel(n_jobs=16)(delayed(plot_chemistry)(file, folder, is_smart)
+        for file in files[0:5]:
+            if ("profiling" not in file) and ("tracker" not in file):
+                plot_chemistry(file, folder, is_smart, status);
+        
+        Parallel(n_jobs=16)(delayed(plot_chemistry)(file, folder, is_smart, status)
                             for file in files if "profiling" not in file)
 
         # Define the command for making videos
@@ -268,7 +339,6 @@ if __name__ == '__main__':
         os.system(ffmpeg_cmd.format('calcite-dolomite'))
         os.system(ffmpeg_cmd.format('aqueous-species'))
         os.system(ffmpeg_cmd.format('ph'))
-        '''
-    # Plot profiling results
-    plot_cpu_times(profiling)
+        
+'''
 
